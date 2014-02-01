39c932cd35f9af88fe74f153368e8652a5d07cab
TreeNode <ondaemon@gmail.com>
2014-02-01 19:42:28 +0200

    setup

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
     name='bitcoin-python3',
-    version='0.3',
+    version='0.3.1',
     description='Friendly Bitcoin JSON-RPC API binding for Python 3',
     long_description='This package allows performing commands such as listing the current balance'
     ' and sending coins to the Satoshi (original) client from Python 3. The communication with the'
